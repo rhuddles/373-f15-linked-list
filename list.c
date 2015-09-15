534bc6d1bb18ad00bc328e48f73da0397c22e942
Bobby <rhuddles@umich.edu>
2015-09-14 20:52:46 -0400

    Homework complete

--- list.c
+++ list.c
@@ -11,6 +11,34 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	list_t* current = head;
+	list_t* last_el = NULL;
+
+	while(new_element->index > current->index){
+		last_el=current;
+		current=current->next;
+		if(current->next == NULL)
+			break;
+	}
+
+	if(current->next != NULL)
+		new_element->next = current;
+	else if(new_element->index > current->index){
+		current->next = new_element;
+		new_element->next=NULL;
+		last_el=current;
+	}
+	else {
+		last_el->next=new_element;
+		new_element->next=current;
+	}
+
+
+	if(last_el != NULL)
+		last_el->next = new_element;
+	else
+		head = new_element;
+
 	return head;
 }
 
@@ -18,6 +46,19 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+
+
+	list_t* last_el = NULL;
+	list_t* next_el = head;
+	list_t* curr_el = head;
+	while(curr_el->next != NULL){
+		next_el=curr_el->next;
+		curr_el->next=last_el;
+		last_el=curr_el;
+		curr_el=next_el;
+	}
+	curr_el->next=last_el;
+	head=curr_el;
 
 	return head;
 }
